c8da83d6f7dc11aff0c81fe900826d44ce8a6bed
Ivanildo Barauna <ivanildo.jnr@outlook.com>
2025-01-30 03:11:50 +0000

    feature: remove get_logger method

--- src/otel_wrapper/domain/services/logs_service.py
+++ src/otel_wrapper/domain/services/logs_service.py
@@ -5,10 +5,7 @@ from ...infrastructure.ports.outbound_logs_exporter import iLogsExporter
 class LogsProcessorService:
     def __init__(self, log_exporter: Type[iLogsExporter]):
         self.exporter = log_exporter
-    
-    def get_logger(self):
-        return self.exporter.get_logger()
-    
+        
     def new_log(self, msg: str, tags: dict, level: int):
         logger = self.get_logger()
         logger.log(level, msg, extra=tags)
